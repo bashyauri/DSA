7d009c5352faef697282e9acedafbc2245a0c596
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-26 17:56:05 +0100

    add search method to binary tree

--- BinaryTreeList.py
+++ BinaryTreeList.py
@@ -1,18 +1,24 @@
 class BinaryTree:
     def __init__(self, size):
-        self.custom_size = size * [None]
+        self.custom_list = size * [None]
         self.last_used_index = 0
         self.size = size
 
     def insert_node(self, value):
         if self.last_used_index + 1 == self.size:
             return "The List is full"
-        self.custom_size[self.last_used_index + 1] = value
+        self.custom_list[self.last_used_index + 1] = value
         self.last_used_index += 1
         return "The value has been successfully inserted"
 
+    def search_node(self, node_value):
+        for i in range(len(self.custom_list)):
+            if self.custom_list[i] == node_value:
+                return "Success"
+        return "Not Found"
+
 
 bst = BinaryTree(8)
-bst.insert_node("Drinks")
-bst.insert_node("Hot")
-bst.insert_node("Right")
+print(bst.insert_node("Drinks"))
+print(bst.insert_node("Hot"))
+print(bst.insert_node("Right"))
